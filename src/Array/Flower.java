2b599b536848509717765d53fc605bc3a69d8a78
Kuhoo Saxena <saxenak.0909@gmail.com>
2022-01-30 23:46:33 +0530

    Create Array of 5 Flower objects and print values.

--- src/Array/Flower.java
+++ src/Array/Flower.java
@@ -1,6 +1,6 @@
 package Array;
 
-//Q. Write a Java class, Flower, that has three instance variables of type String, int, and float,
+//        Q. Write a Java class, Flower, that has three instance variables of type String, int, and float,
 //        which respectively represent the name of the flower,its number of petals, and price.
 //        Your class must include a constructor method that initialises each variable to an appropriate
 //        value, and your class should include methods for setting the value of each type, and
